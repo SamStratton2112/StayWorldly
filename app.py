59424b7759a865a744fbce526b99467d95dacbfd
SamStratton2112 <109560024+SamStratton2112@users.noreply.github.com>
2023-11-07 10:35:38 -0800

    Update cities list

--- app.py
+++ app.py
@@ -43,6 +43,9 @@ def homepage():
         if city.city_name not in all_user_cities:
             all_user_cities.append(city.city_name)
             user_cities.insert(0, city)
+        else: 
+            user_cities.remove(city)
+            user_cities.insert(0, city)
     all_user_cities = user_cities[:9:]
     form = SearchForm()
     if form.validate_on_submit():
