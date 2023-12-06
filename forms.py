2a89246bd299ba554f40ddea30fb7b9af50b432d
SamStratton2112 <109560024+SamStratton2112@users.noreply.github.com>
2023-12-06 13:23:38 -0800

    Update forms

--- forms.py
+++ forms.py
@@ -5,6 +5,13 @@ from wtforms.validators import DataRequired, Length
 class SearchForm(FlaskForm):
     """search city form"""
     city = StringField('City', validators=[DataRequired()])
+    
+class CommentForm(FlaskForm):
+    """comment Form"""
+
+    username = StringField('Username', validators=[DataRequired()])
+    city_name = StringField('City Name', validators=[DataRequired()])
+    comment = TextAreaField('Comment', validators=[DataRequired()])
 
 class LoginForm(FlaskForm):
     """Login form."""
