6e608eb4c84d385c465642579580abdb1e2fe97a
SamStratton2112 <109560024+SamStratton2112@users.noreply.github.com>
2024-01-11 14:31:09 -0800

    Finalize

--- app.py
+++ app.py
@@ -241,6 +241,9 @@ def show_city(city):
         # pull numbers out of string 
         user_tz = user_tz_str[0:3] if user_tz_str[0] == '-' else user_tz_str[1:3]
         # calculate time difference 
+        if len(user_tz)==2:
+            # handle negative tz
+            time_dif = (-1 * int(user_tz[1:])) - int(tzoffset)
         time_dif = int(user_tz) - int(tzoffset)
 
         #Get Country data using the country the city is in from teleport api 
